6a19debb38b9f30a50def0a87d8fc1c2aa4b7878
Yashh1214 <144115697+Yashh1214@users.noreply.github.com>
2025-07-25 00:23:58 +0530

    Update App.jsx

--- ZCoder--main/src/App.jsx
+++ ZCoder--main/src/App.jsx
@@ -1,30 +1,3 @@
-// import React from 'react'
-//  import { BrowserRouter,Routes,Route } from 'react-router-dom'
-// import Home from './pages/Home/Home'
-// import Login from './pages/LoginPage/Login'
-// import Nav from './components/Nav/Nav'
-// import Signup from './pages/SignupPage/Signup'
-// import Footer from './components/Footer/Footer'
-// import './App.css'
-// const App = () => {
-//   return (
-//   <> 
-//     <BrowserRouter>
-//     <div className="app">
-//       <Nav/>
-//       <Routes>
-//         <Route path='/' element={<Home/>}/>
-//         <Route path='/login' element={<Login/>}/> 
-//         <Route path='/signup' element={<Signup/>}/>
-//       </Routes>
-//       <Footer/>
-//     </div>
-//     </BrowserRouter>
-//   </>
-//   )
-// }
-
-// export default App
 
 
 
