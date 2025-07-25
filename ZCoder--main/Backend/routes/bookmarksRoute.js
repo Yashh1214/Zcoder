f48c2bbc56e9ea2b6e538150298d0f51a464d10b
Yashh1214 <144115697+Yashh1214@users.noreply.github.com>
2025-07-25 10:07:52 +0530

    Update bookmarksRoute.js

--- ZCoder--main/Backend/routes/bookmarksRoute.js
+++ ZCoder--main/Backend/routes/bookmarksRoute.js
@@ -7,5 +7,4 @@ const router = express.Router();
 router.post('/bookmarks', addBookmark);
 router.delete('/bookmarks/:userId/:titleSlug/:id',deleteBookmark);
 router.get('/bookmarks/:userId',getBookmarks);
-// router.get('/bookmarks/:titleSlug/:userId', getBookmarkedSolutionsByQuestion);
 module.exports = router;
