4cc5ebda19e7054dcd30bd09460fd2c7fb68fc46
Yashh1214 <144115697+Yashh1214@users.noreply.github.com>
2025-07-25 10:09:50 +0530

    Update Nav.jsx

--- ZCoder--main/src/components/Nav/Nav.jsx
+++ ZCoder--main/src/components/Nav/Nav.jsx
@@ -1,34 +1,3 @@
-// import React,{useState}from 'react'
-// import { NavLink } from 'react-router-dom'
-// import { GiHamburgerMenu } from "react-icons/gi"
-// import './Nav.css'
-// const Nav = () => {
-//   const[menuOpen,setMenuOpen] = useState(false);
-//   return (
-//       <div className="nav-bar">
-//         <div className="logo">
-//           <h1>Zcoder</h1>
-//         </div>
-//         <div className="hamburger-menu" onClick={()=>{setMenuOpen(!menuOpen);}}>
-//           <a href="#" ><GiHamburgerMenu /></a>
-//         </div>
-        
-//         <ul className={menuOpen?"open":""}>
-//           <li><NavLink to='/contests'>Contests</NavLink></li>
-//           <li><NavLink to='/practice'>Practice</NavLink></li>
-//           <li><NavLink to='/submissions'>Submissions</NavLink></li>
-//           <li><NavLink to='/bookmarks'>Bookmarks</NavLink></li>
-//           <li><NavLink to='/login'>Login</NavLink></li>
-//           <li><NavLink to='/signup'>Signup</NavLink></li>
-//         </ul>
-          
-        
-//       </div>
-
-//   )
-// }
-// export default Nav
-
 
 import React,{useState,useContext}from 'react'
 import { NavLink } from 'react-router-dom'
@@ -42,10 +11,6 @@ const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false)
   const {logout} = useLogout()
   const {isAuthenticated,userLogin,isEdit,user} = useAuthContext()
-
-  // const userInfo = JSON.parse(localStorage.getItem("user"));
-  // console.log(userInfo)
-  // const { state, dispatch } = useContext(AuthContext)
   const handleLogOut = () =>{
     logout()
   }
